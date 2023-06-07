9aac518eb306dbbcb630785a4e36dc2386afcbf0
Jaycode88 <>
2023-06-07 10:31:15 +0000

    Addition function complete

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,4 +1,15 @@
-function addition(num1, num2) {
+function addition(num1, num2,) {
+    if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return NaN;
+    }
     return num1 + num2;
 }
 module.exports = addition;
+
+//solution for last test on addition function below....
+//function addition(...numbers) {
+//    if (numbers.some((num) => typeof num !== "number")) {
+//        return NaN;
+//    }
+//    return numbers.reduce((sum, num) => sum + num, 0);
+//}
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -8,6 +8,45 @@ describe("Calculator", () => {
         test("should return 73 for 42 + 31", () => {
             expect(addition(42, 31)).toBe(73);
         });
+        test("should return NaN for no parameters", () => {
+            expect(addition()).toBeNaN();
+        });
+        test("should return NaN for missing 1 parameter", () => {
+            expect(addition(20)).toBeNaN();
+        });
+        test("should return NaN for string parameters", () => {
+            expect(addition("20", "25")).toBeNaN();
+        });
+        test("should return NaN for mixed string and number parameters", () => {
+            expect(addition("42", 31)).toBeNaN();
+        });
+        test("should return NaN for null parameters", () => {
+            expect(addition(null, null)).toBeNaN();
+        });
+        test("should return NaN for mixed null and number parameters", () => {
+            expect(addition(null, 31)).toBeNaN();
+        });
+        test("should return NaN for undefined parameters", () => {
+            expect(addition(undefined, undefined)).toBeNaN();
+        });
+        test("should return NaN for mixed undefined and number parameters", () => {
+            expect(addition(undefined, 31)).toBeNaN();
+        });
+        test("should return 8.6491 for 6.1379 + 2.5112", () => {
+            expect(addition(6.1379, 2.5112)).toBe(8.6491);
+        });
+        test("should return 9.95354 for 7.96734 + 1.9862", () => {
+            expect(addition(7.96734, 1.9862)).toBe(9.95354);
+        });
+        test("should return -2 for -6 + 4", () => {
+            expect(addition(-6, 4)).toBe(-2);
+        });
+        test("should return -12 for -6 + -6", () => {
+            expect(addition(-6, -6)).toBe(-12);
+        });
+        // test("should return 15 for 5 + 5 + 5", () => {        // do the same tests for each function before adding this test and ammending code
+        //    expect(addition(5, 5, 5)).toBe(15);               // as to pass this test you need to use reduce and you need to understand that better yet.
+        //});
     });
     describe("Subtract function", () => {
 
