55f7b521541b3d7617dbd7c030d2d4ea65987a92
Jaycode88 <>
2023-06-07 09:51:52 +0000

    Second test complete

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -5,6 +5,9 @@ describe("Calculator", () => {
         test("should return 42 for 20 + 22", () => {
             expect(addition(20, 22)).toBe(42);
         });
+        test("should return 73 for 42 + 31", () => {
+            expect(addition(42, 31)).toBe(73);
+        });
     });
     describe("Subtract function", () => {
 
@@ -16,3 +19,14 @@ describe("Calculator", () => {
 
     });
 });
+
+// think about what other tests need to be done to make the function robust,
+// Below is a list of tests that could be done.
+// Test for no parameters
+// test for missing parameters
+// a string as 1 or both parameters
+//null as 1 or both parameters
+//undefined as a parameter
+//does it work with float point numbers
+//does it work with negative numbers
+//what about 3 or more parameters
